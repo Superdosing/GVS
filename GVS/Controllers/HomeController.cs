a97b04f7eae8c63944025da4376b70dba35ef6b8
Danni Rosing <rosingdanni@gmail.com>
2017-03-23 13:50:34 +0100

    Begyndene setup. DON'T TOUCH YET!!

---
+++ GVS/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GVS.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Priser()
+        {
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+
+        public ActionResult Logind()
+        {
+            return View();
+        }
+    }
+}
